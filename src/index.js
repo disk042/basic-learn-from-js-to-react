d514f2011fc2f6265201f6f9727b3d4bc76e322d
DaisukeTashiro <62758430+disk042@users.noreply.github.com>
2021-04-04 11:56:01 +0900

    more learn allow function

--- src/index.js
+++ src/index.js
@@ -74,5 +74,10 @@
 // アロー関数
 const func2 = (str) => {
   return str;
-}
+};
 console.log(func2("func2です。"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(20, 10));
