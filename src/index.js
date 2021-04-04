61283d9d265ad7638668a453d45f634411b2143f
DaisukeTashiro <62758430+disk042@users.noreply.github.com>
2021-04-04 22:01:26 +0900

    learn map and fileter

--- src/index.js
+++ src/index.js
@@ -142,3 +142,33 @@
 // console.log(arr7);
 
 // arr8 = arr4 で参照渡しをしてしまうと、新たに値を入れた際に挙動がおかしくなるので注意
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+const nameArr = ["田中", "山田", "佐藤"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index}番目は${nameArr[index]}です`);
+// }
+
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 0;
+// });
+// console.log(newNumArr);
+
+const newNameArr = nameArr.map((name) => {
+  if (name === "佐藤") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+console.log(newNameArr);
