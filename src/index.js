a855a63d2159611142712f2db49f69677f6dccaa
DaisukeTashiro <62758430+disk042@users.noreply.github.com>
2021-04-04 21:51:12 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -111,3 +111,34 @@
  */
 // const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
 // sayHello("daisuke");
+
+/**
+ * スプレッド構文
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// // sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//  まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// arr8 = arr4 で参照渡しをしてしまうと、新たに値を入れた際に挙動がおかしくなるので注意
